e9474abb87bc07137d2f38ec29ca1f8797cae691
shemmink <shemmink@cs.helsinki.fi>
2015-02-17 16:16:45 +0200

    Added classifiers.json to main/resources/ - Samuli

--- src/main/java/ehdotus/estimators/ImplemationUno.java
+++ src/main/java/ehdotus/estimators/ImplemationUno.java
@@ -9,6 +9,18 @@ public class ImplemationUno implements Estimator {
     @Override
     public void estimateDifficulty(DifficultyData data) {
         // ...
+    	
+    	
+    	// 1. General classifier
+    	// - Just load the classifier structure from file & predict
+    	
+    	// 2. User-Cluster specific classifier
+    	// - Get userID and associate with a cluster
+    	// - Load cluster-specific classifier from file & predict
+    	
+    	
+    	
+    	
     }
     
 }
